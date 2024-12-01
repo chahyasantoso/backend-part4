a810770c1c2167d77ac6863d76aa46e9a655b5c8
chahyasantoso <chahya.santoso@gmail.com>
2024-12-01 18:23:50 +0700

    update blogs router to return  populated values

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -4,37 +4,41 @@ const middleware = require('../utils/middleware')
 
 
 router.get("/", async (request, response) => {
-  const blogs = await Blog.find({}).populate('user', { username: 1, name: 1})
+  const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
   response.json(blogs)
 })
 
-router.post("/", middleware.userExtractor, async (request, response) => {  
+router.post("/", middleware.userExtractor, async (request, response) => {
   const user = request.user
   if (!user) {
     return response.status(401).json({ error: 'token invalid' })
   }
 
-  const blog = new Blog({...request.body, user: user._id})
+  const blog = new Blog({ ...request.body, user: user._id })
   const savedBlog = await blog.save()
+  await savedBlog.populate('user', { username: 1, name: 1 })
 
   user.blogs = [...user.blogs, savedBlog._id]
   await user.save()
-  
+
   response.status(201).json(savedBlog)
 })
 
 router.put("/:id", async (request, response) => {
-  const updatedBlog = new Blog({...request.body})
+  const updatedBlog = new Blog({ ...request.body })
   await updatedBlog.validate()
 
   const result = await Blog.findByIdAndUpdate(
-    request.params.id, 
-    {...request.body}, 
-    {new: true, runValidators: true, context: 'query'}
-  )
+    request.params.id,
+    { ...request.body },
+    { new: true, runValidators: true, context: 'query' }
+  ).populate('user', { username: 1, name: 1 })
+
   if (!result) {
     return response.status(404).end()
   }
+
+  console.log(result)
   response.json(result)
 })
 
@@ -55,6 +59,7 @@ router.delete("/:id", middleware.userExtractor, async (request, response) => {
 
   user.blogs = user.blogs.filter(blogId => blogId.toString() !== blog._id.toString())
   await user.save()
+  //doesn't need populate, i don't think it's in use
 
   await blog.deleteOne()
   return response.json(blog)

2ac851426e3be66babcf7ba8aa3907e21eb152f5
chahyasantoso <chahya.santoso@gmail.com>
2025-01-21 11:07:02 +0700

    spell corrected middleware.js for token error

--- controllers/users.js
+++ controllers/users.js
@@ -1,9 +1,13 @@
-const router = require("express").Router()
-const User = require("../models/user")
-const bcrypt = require("bcryptjs")
+const router = require('express').Router()
+const User = require('../models/user')
+const bcrypt = require('bcryptjs')
 
-router.get("/", async (request, response) => {
-  const users = await User.find({}).populate('blogs', {title: 1, author: 1, url: 1})
+router.get('/', async (request, response) => {
+  const users = await User.find({}).populate('blogs', {
+    title: 1,
+    author: 1,
+    url: 1,
+  })
   response.json(users)
 })
 
@@ -14,19 +18,19 @@ function validatePassword(password) {
   return true
 }
 
-router.post("/", async (request, response) => {
-  const {username, password, name} = request.body
+router.post('/', async (request, response) => {
+  const { username, password, name } = request.body
 
   if (!validatePassword(password)) {
-    return response.status(400).json({ error: 'password must not be < 3' });
+    return response.status(400).json({ error: 'password must not be < 3' })
   }
-  
+
   const salt = 10
   const passwordHash = await bcrypt.hash(password, salt)
   const user = new User({
     username,
     password: passwordHash,
-    name
+    name,
   })
 
   const result = await user.save()
